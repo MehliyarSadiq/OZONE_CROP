b92de6ea914c2497ce31dadcaee97db2390ce75f
Mehliyar <mehliyar@gmail.com>
2020-02-05 11:41:15 +0000

    update

--- code/calc_crop_production_interpolation_save.R
+++ code/calc_crop_production_interpolation_save.R
@@ -21,14 +21,25 @@ latlon = cbind(lat.array, lon.array)
 
 for(i in 1:4) {
   plot.field(prod[,,i], lon.0.5, lat.0.5)
-  prod.array = array(prod[,,i])
-  prod_2degree[,,i] = sp.regrid(spdata = prod[,,i], lon.in = lon.0.5, lat.in = lat.0.5, lon.out = lon, lat.out = lat)
+  print(paste(crop.name[i], 'before regriding:', sum(prod[,,i], na.rm = T)))
+  
+  for(ilat in 1:(length(lat)-1))
+    for(ilon in 1:length(lon)){ # summing up grids belong to coarser grid
+      subset.0.5 = prod[(1+5*(ilon-1)):(5*ilon),(1+4*(ilat-1)):(4*ilat),i]
+      prod_2degree[ilon,ilat,i] = sum(subset.0.5, na.rm = T)
+    }
+  #prod.array = array(prod[,,i])
+  #prod_2degree[,,i] = sp.regrid(spdata = prod[,,i], lon.in = lon.0.5, lat.in = lat.0.5, lon.out = lon, lat.out = lat)
+  #print(paste(crop.name[i], 'after regriding:', sum(prod_2degree[,,i], na.rm = T)))
   #prod_2degree[,,i] = spavg(spdata = prod.array, latlon = latlon, lat.frame = lat, lon.frame = lon)
+  print(paste(crop.name[i], 'after regriding:', sum(prod_2degree[,,i], na.rm = T)))
   plot.field(prod_2degree[,,i], lon, lat)
 }
 crop.list = c('Wheat', 'Rice', 'Maize', 'Soybean')
 for(i in 1:4) assign(x = paste0('prod_', crop.list[i]), value = prod_2degree[,,i])
 
 setwd('~/Desktop/analysis/data/crop')
-save(list = c('prod_Maize', 'prod_Rice', 'prod_Soybean', 'prod_Wheat'), file = 'crop_prod.RData')
+unit.info = 'crop.prod: ton/grid cell'
+crop.types = crop.list
+save(list = c('prod_Maize', 'prod_Rice', 'prod_Soybean', 'prod_Wheat', 'crop.types', 'unit.info'), file = 'crop_prod.RData')
 
