6c33c26ddd95dfd7b1122b884fd39c6159ae49ef
Mehliyar <mehliyar@gmail.com>
2020-02-05 22:28:45 +0000

    udpate

--- code/2.calc_crop_prod_plot.R
+++ code/2.calc_crop_prod_plot.R
@@ -1,44 +1,69 @@
 # calculate crop production
 library(fields)
 source('~/Desktop/analysis/code/get_geo.R')
-lat = seq(-90,90,2)
-lon = seq(-180,177.5,2.5)
 
+# FAO crop data, 0.5 degree resolution
 load('~/Desktop/analysis/data/crop/crop.FAO-USDA.hres.0.5x0.5.1961-2010.RData')
 
-prod = array(NaN, dim=dim(crop.yield.5yma[,,50,]))
-for(i in 1:4) prod[,,i] = crop.yield.5yma[,,50,i] * harvest.area[,,50,i]
-for(i in 1:4) plot.field(prod[,,i], lon.0.5, lat.0.5)
+crop_prod = crop.yield*harvest.area # [lon, lat, years, crops]
+dim(crop_prod)
 
-prod_2degree = array(NaN, dim=c(144,91,4))
+# 2006-2010 mean crop production
+crop_prod_5year_mean = apply(X = crop_prod[,,46:50,], FUN = mean, MARGIN = c(1,2,4), na.rm = T)
+dim(crop_prod_5year_mean)
 
-lonlon = replicate(length(lat.0.5), lon.0.5)
-latlat = t(replicate(length(lon.0.5), lat.0.5))
-lon.array = array(lonlon)
-lat.array = array(latlat)
-latlon = cbind(lat.array, lon.array)
+# lat and lon of TEMIR outputs
+lat = seq(-90,90,2)
+lon = seq(-180,177.5,2.5)
+# output grid
+prod_2degree = array(NaN, dim=c(length(lon),length(lat),length(crop.name)))
 
-for(i in 1:4) {
-  plot.field(prod[,,i], lon.0.5, lat.0.5)
-  print(paste(crop.name[i], 'before regriding:', sum(prod[,,i], na.rm = T)))
-  
-  for(ilat in 1:(length(lat)-1))
-    for(ilon in 1:length(lon)){ # summing up grids belong to coarser grid
-      subset.0.5 = prod[(1+5*(ilon-1)):(5*ilon),(1+4*(ilat-1)):(4*ilat),i]
-      prod_2degree[ilon,ilat,i] = sum(subset.0.5, na.rm = T)
+# split lon grids in half, for regridding
+crop_prod_0.5x0.25 = array(NaN, dim=c(2*length(`lon.0.5`),length(`lat.0.5`),4))
+dim(crop_prod_0.5x0.25)
+sum(crop_prod_5year_mean[,,3], na.rm = T) # global total prod. in tonnes
+for(ilon in 1:(2*length(`lon.0.5`))){
+    lon.ind = as.integer((ilon+1)/2)
+    crop_prod_0.5x0.25[ilon,,] = 0.5*crop_prod_5year_mean[lon.ind,,]
     }
-  #prod.array = array(prod[,,i])
-  #prod_2degree[,,i] = sp.regrid(spdata = prod[,,i], lon.in = lon.0.5, lat.in = lat.0.5, lon.out = lon, lat.out = lat)
-  #print(paste(crop.name[i], 'after regriding:', sum(prod_2degree[,,i], na.rm = T)))
-  #prod_2degree[,,i] = spavg(spdata = prod.array, latlon = latlon, lat.frame = lat, lon.frame = lon)
-  print(paste(crop.name[i], 'after regriding:', sum(prod_2degree[,,i], na.rm = T)))
-  plot.field(prod_2degree[,,i], lon, lat)
-}
-crop.list = c('Wheat', 'Rice', 'Maize', 'Soybean')
-for(i in 1:4) assign(x = paste0('prod_', crop.list[i]), value = prod_2degree[,,i])
+sum(crop_prod_0.5x0.25[,,3], na.rm = T)
 
+# before and after regridding
+before = crop_prod_0.5x0.25
+after = prod_2degree
+
+for(icrop in 1:length(crop.name)){
+    print(paste('global total production', crop.name[icrop], 'before regriding:', sum(before[,,icrop], na.rm = T)))
+
+    for(ilat in 1:length(lat))
+    for(ilon in 1:length(lon)){ # summing up grids belong to coarser grid
+        
+        # get the subset of data in finer grid that falls into coarser grid
+        
+        # 1, get lat indices
+        if(ilat == 1) lat.ind = c(1,2) # first and last box contains only 2 finer grids
+        else if(ilat == length(lat)) lat.ind = c(length(`lat.0.5`)-1,length(`lat.0.5`))
+        # ilat = 2, lat.ind = 3,4,5,6, ilat = 3, lat.ind = 7,8,9,10
+        else lat.ind = (3+(ilat-2)*4):(2+(ilat-1)*4)
+        #print(lat.ind)
+        
+        # 2, get lon indices
+        if(ilon == 1) lon.ind = c(1:5,(2*length(`lon.0.5`)-4):(2*length(`lon.0.5`))) 
+        #ilon = 2, lon.ind = 6:15, ilon = 3, lon.ind = 16:25
+        else lon.ind = (6+(ilon-2)*10):(5+(ilon-1)*10)
+        
+        subset = before[lon.ind,lat.ind,]
+        after[ilon,ilat,icrop] = sum(subset[,,icrop], na.rm = T) # sum up the subset of data
+    }
+    print(paste('global total production', crop.name[icrop], 'after regriding:', sum(after[,,icrop], na.rm = T)))
+    #plot.field(after[,,i], lon, lat)
+}
+prod_2degree = after
+
+crop.types = c('Wheat', 'Rice', 'Maize', 'Soybean')
+for(i in 1:4) assign(x = paste0('prod_', crop.types[i]), value = prod_2degree[,,i])
+# save data
 setwd('~/Desktop/analysis/data/crop')
 unit.info = 'crop.prod: ton/grid cell'
-crop.types = crop.list
 save(list = c('prod_Maize', 'prod_Rice', 'prod_Soybean', 'prod_Wheat', 'crop.types', 'unit.info'), file = 'crop_prod.RData')
 
---
+++ code/3.calc_prod_energy_loss.R
@@ -0,0 +1,115 @@
+# energy loss
+library(fields); library(ggplot2)
+source('~/Desktop/analysis/code/get_geo.R')
+
+load('~/Desktop/analysis/data/crop/crop_prod.RData')
+lat = seq(-90,90,2)
+lon = seq(-180,177.5,2.5)
+
+expo.metrics = c('AOT40', 'POD3_DOSE', 'POD3_FBB', 'W126')
+
+# total food production energy
+for(i in 1:length(crop.types)){
+  prod = get(paste0('prod_', crop.types[i])) # map: ton per grid cell
+  prod.total = sum(prod, na.rm = T)         # number: total yield (tons)
+  print(paste(crop.types[i], 'total food prodcution energy (1e12 kcal)',prod.total*unit_conver[i]*1e-6))
+}
+
+for(j in 1:length(expo.metrics))
+{
+load(paste0('~/Desktop/analysis/data/crop/ry/ry_', expo.metrics[j], '.RData'))
+# prod_crop_name, ton per grid cell
+# ry_crop_name
+unit_conver = c(3.65, 3.34, 4.46, 3.63) # 10^6 kcal ton-1
+prod.loss.per = rep(0, 4)
+ener.loss = rep(0, 4)
+
+for(i in 1:length(crop.types)){
+  prod = get(paste0('prod_', crop.types[i])) # map: ton per grid cell
+  prod.total = sum(prod, na.rm = T)         # number: total yield (tons)
+
+  prod_untact = prod / get(paste0('ry_', crop.types[i], '_', expo.metrics[j])) # map: theoretical production, unaffected by ozone
+  prod.theo = sum(prod_untact, na.rm = T) # number: theoretical total production
+  prod_loss = prod - prod_untact       # map: production loss (tons)
+  prod.loss = sum(prod_loss, na.rm = T) # number: total loss (tons)
+  plot.field(prod_loss, lon , lat, col = rev(tim.colors(n=32)[17:32]), type = 'def', zlim = c(-40000,0))
+  
+  print(paste(expo.metrics[j], crop.types[i], 'prod loss (%): ', 100*prod.loss/prod.theo))
+  
+  prod.loss.per[i] = -100*prod.loss/prod.theo # number: prod loss (%)
+  ener.loss[i] = prod.loss * unit_conver[i]/(1e6) # energy loss(10e12kcal)
+}
+assign(x = paste0('df_',expo.metrics[j]), value = data.frame(crop.types, method = expo.metrics[j], pl = prod.loss.per, el = ener.loss))
+}
+
+# m7 and m12
+load('~/Desktop/analysis/data/crop/ry/ry_m7-m12.RData')
+pl_maize = prod_Maize / ry_Maize_M12 - prod_Maize # production loss map: ton per grid cell
+pl.maize = sum(pl_maize, na.rm = T)/sum(prod_Maize / ry_Maize_M12, na.rm = T) # number: ton
+print(paste('M12 Maize yield loss (%) ', -100*pl.maize))
+el.maize = pl.maize * unit_conver[1] * 1e-6 # energy loss(10e12kcal)
+
+pl_wheat = prod_Wheat / ry_Wheat_M7 - prod_Wheat
+pl.wheat = sum(pl_wheat, na.rm = T)/sum(prod_Wheat / ry_Wheat_M7, na.rm = T)
+print(paste('M7 Wheat yield loss (%) ', -100*pl.wheat))
+el.wheat = pl.wheat * unit_conver[2] * 1e-6 # energy loss(10e12kcal)
+
+pl_soybean = prod_Soybean / ry_Soybean_M12 - prod_Soybean
+pl.soybean = sum(pl_soybean, na.rm = T)/sum(prod_Soybean / ry_Soybean_M12, na.rm = T)
+print(paste('M12 Soybean yield loss (%) ', -100*pl.soybean))
+el.soybean = pl.soybean * unit_conver[3] * 1e-6 # energy loss(10e12kcal)
+
+pl_rice = prod_Rice / ry_Rice_M7 - prod_Rice
+pl.rice = sum(pl_rice, na.rm = T)/sum(prod_Rice / ry_Rice_M7, na.rm = T)
+print(paste('M7 Rice yield loss (%) ', -100*pl.rice))
+el.rice = pl.rice * unit_conver[4] * 1e-6 # energy loss(10e12kcal)
+
+ener.loss = c(el.maize, el.wheat, el.soybean, el.rice)
+print(ener.loss)
+ener_sum = sum(el.maize, el.wheat, el.soybean, el.rice)
+
+prod.loss.per = 100*c(pl.maize, pl.wheat, pl.soybean, pl.rice)
+assign(x = 'df_m', value = data.frame(crop.types, method = 'M7/M12', pl = prod.loss.per, el = ener.loss))
+
+# combine all dfs
+df_all = rbind(df_AOT40, df_m, df_W126, df_POD3_DOSE, df_POD3_FBB)
+
+p = ggplot(df_all, aes(y = pl, x = crop.types)) +
+  geom_bar(stat='identity', fill = 'steelblue') +
+  geom_text(aes(label=format(pl, digits = 1)), vjust=1.6, color="white", size=3.5) +
+  facet_wrap(~method) +
+  labs(x = ' ', y = 'Production loss (%)') #, title = "Global average"
+print(p)
+
+p = ggplot(df_all, aes(x = crop.types, y = -el)) + 
+  geom_bar(stat='identity', fill = 'steelblue') +
+  geom_text(aes(label=format(pl, digits = 1)), vjust=1.6, color="white", size=3.5) +
+  facet_wrap(~method) +
+  labs(x = ' ', y = 'Energy loss (^12 kcal)') #, title = "Global average"
+  print(p)
+  # geom_point(aes(shape = method), size = 3.5, position = position_dodge(0.4)) +
+  # scale_shape_manual(values = c(4,7,10,16,17)) +
+  # theme(legend.title = element_blank()) +
+  # labs(x = ' ', y = 'Energy loss (^12 kcal)') + #, title = "Global average"
+  # print(p)
+
+# p = ggplot(df_all, aes(y = yield.loss.avg, x = method)) +
+#   geom_bar(stat='identity', fill = 'steelblue') +
+#   geom_text(aes(label=format(yield.loss.avg, digits = 1)), vjust=1.6, color="white", size=3.5) +
+#   facet_wrap(~crop.types) +
+#   labs(x = ' ', y = 'Yield loss (%)', title = "Global average")
+# print(p)
+# 
+mills = data.frame(crop.types = crop.types, method = 'Mills et al. 2018', pl = c(6.1, 7.1, 12.4, 4.4), el = rep(0,4))
+df_all = rbind(df_all, mills)
+
+p = ggplot(df_all, aes(x = crop.types, y = pl, group = method)) + 
+  geom_point(aes(shape = method), size = 3.5, position = position_dodge(0.4)) +
+  scale_shape_manual(values = c(4,7,10,16,17,22)) +
+  theme(legend.title = element_blank()) +
+  labs(x = ' ', y = 'Production loss (%)') + #, title = "Global average"
+  geom_segment(aes(x = 'Maize', y = 2.2, xend = 'Maize', yend = 5.5, linetype = 'Ainsworth et al. 2017'), color = "blue") +
+  geom_segment(aes(x = 'Rice', y = 3, xend = 'Rice', yend = 4), color = "blue") +
+  geom_segment(aes(x = 'Soybean', y = 5.4, xend = 'Soybean', yend = 15.6), color = "blue") +
+  geom_segment(aes(x = 'Wheat', y = 3.9, xend = 'Wheat', yend = 15.4), color = "blue")
+print(p)
